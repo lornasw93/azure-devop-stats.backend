d1794c92a05b1de843168b95e07c5b860d3f0aca
lornasw93 <lornnaa@live.com>
2020-02-26 13:22:50 +0000

    update

--- DevOpsStats.Api/Startup.cs
+++ DevOpsStats.Api/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
 namespace DevOpsStats.Api
@@ -64,7 +65,7 @@ namespace DevOpsStats.Api
             });
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
